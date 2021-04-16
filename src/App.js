672ba44c297d071d751d070b70e373a0904c5245
Sean Grant <sgrant177@yahoo.com>
2021-04-16 10:53:41 -0400

    better responisveness

--- src/App.js
+++ src/App.js
@@ -21,27 +21,27 @@ function App() {
         <Header />
       </div>
       <div id="myInfo" className="row">
-        <div id="portrait" className="col-4">
+        <div id="portrait" className="col-xl-4 col-lg-12 col-md-12">
           <Portrait />
         </div>
-        <div id="brandStatement" className="col-8">
+        <div id="brandStatement" className="col-xl-8 col-lg-12 col-md-12">
           <Brandstatement />
         </div>
       </div>
       <div id="projects">
         <div className="row">
-          <div id="turnup" className="col-lg-6 col-md-12">
+          <div id="turnup" className="col-xl-6 col-lg-12">
             <Turnup />
           </div>
-          <div id="googlebooks" className="col-lg-6 col-md-12">
+          <div id="googlebooks" className="col-xl-6 col-lg-12">
             <Googlebooks/>
           </div>
         </div>
         <div className="row">
-          <div id="fitnesstracker" className="col-lg-6 col-md-12">
+          <div id="fitnesstracker" className="col-xl-6 col-lg-12">
             <Fitnesstracker/>
           </div>
-          <div id="budgettracker" className="col-lg-6 col-md-12">
+          <div id="budgettracker" className="col-xl-6 col-lg-12">
             <Budgettracker/>
           </div>
         </div>
